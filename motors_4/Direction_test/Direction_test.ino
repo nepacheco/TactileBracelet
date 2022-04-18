f16316efafa48f61235c0ce80f146604f60a2e2d
nepacheco <nepacheco@wpi.edu>
2022-04-18 18:54:37 -0400

    random seed and does intermediate directions

--- motors_4/Direction_test/Direction_test.ino
+++ motors_4/Direction_test/Direction_test.ino
@@ -2,7 +2,8 @@
 int motor_pins[8] = {3,4,5,6,7,8,9,10};
 int timer_stop_pin = 2; // Needs to be an interrupt pin
 int start_button = 11;
-int motor_num = 0;
+int motor_nums[2] = {0,0};
+int motor_temp = 0;
 int run_time = 1000;
 
 
@@ -30,6 +31,7 @@ void setup() {
 }
 
 void loop() {
+  randomSeed(micros());
   // put your main code here, to run repeatedly:
   switch (state) {
     case wait:
@@ -37,18 +39,24 @@ void loop() {
       if (digitalRead(start_button) == LOW) {
         // select a random motor
         state = run_motor;
-        motor_num = random(0,8);
-        double temp = floor(motor_num/2.0)*2;
-        motor_num = temp;
-        Serial.print("Running motor ");
-        Serial.println(motor_num);
+        motor_temp = random(0,8);
+        motor_nums[0] = (int) floor(motor_temp/2.0)*2;
+        motor_nums[1] = (int) (ceil(motor_temp/2.0)*2) % 8 ; // need to mod by 8 so that for a 7 we get 6 and 0
       }
       break;
     case run_motor:
       // just run the motor
-      digitalWrite(motor_pins[motor_num], HIGH);
+      Serial.print("Running motors ");
+      Serial.print(motor_nums[0]);
+      Serial.print(" and ");
+      Serial.println(motor_nums[1]);
+      
+      digitalWrite(motor_pins[motor_nums[0]], HIGH);
+      digitalWrite(motor_pins[motor_nums[1]], HIGH); 
       delay(run_time);
-      digitalWrite(motor_pins[motor_num], LOW);
+      digitalWrite(motor_pins[motor_nums[0]], LOW);
+      digitalWrite(motor_pins[motor_nums[1]], LOW);
+      
       state = wait;
       break;
     default:
